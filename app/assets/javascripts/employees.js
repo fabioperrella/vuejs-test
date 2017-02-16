bbf060c5c4bd4af16d7e2ba5e48c44a85a61fcc2
Fabio Perrella <fabio.perrella@locaweb.com.br>
2017-02-15 23:45:23 -0200

    employee index

---
+++ app/assets/javascripts/employees.js
@@ -0,0 +1,16 @@
+var employees = new Vue({
+  el: '#employees',
+  data: {
+    employees: []
+  },
+  mounted: function() {
+    var that;
+    that = this;
+    $.ajax({
+      url: '/employees.json',
+      success: function(res) {
+        that.employees = res;
+      }
+    });
+  }
+});
---
+++ db/migrate/20170216012335_create_employes.rb
@@ -0,0 +1,11 @@
+class CreateEmployes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :email
+      t.boolean :manager
+
+      t.timestamps null: false
+    end
+  end
+end
