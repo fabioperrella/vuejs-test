693469864a6343399c29b27e653f925d19721e61
Fabio Perrella e Gustavo Santana <fabio.perrella@locaweb.com.br>
2017-02-20 21:19:51 -0300

    edit employee

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -2,6 +2,38 @@ Vue.component('employee-row', {
   template: '#employee-row',
   props: {
     employee: Object
+  },
+  data: function () {
+    return {
+      editMode: false,
+      errors: {}
+    }
+  },
+  methods: {
+    // toggle the manager status which also updates the employee in the database
+    toggleManagerStatus: function () {
+      this.employee.manager = !this.employee.manager
+      this.updateEmployee()
+    },
+    // ajax call for updating an employee
+    updateEmployee: function () {
+      var that = this;
+      $.ajax({
+        method: 'PUT',
+        data: {
+          employee: that.employee,
+        },
+        url: '/employees/' + that.employee.id + '.json',
+        success: function(res) {
+          that.errors = {}
+          that.employee = res
+          that.editMode = false
+        },
+        error: function(res) {
+          that.errors = res.responseJSON.errors
+        }
+      })
+    }
   }
 })
 
